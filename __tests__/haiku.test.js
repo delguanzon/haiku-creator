1a42d277c39ce2fe25f2344a6d9baf266ba86327
delguanzon <yodelguanzon@gmail.com>
2022-11-09 13:33:29 -0800

    Add test for creating haiku object
    Co-authored-by: VW <vera.weikel@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,12 @@
+import Haiku from './../src/js/haiku.js';
+
+describe('Haiku', () => {
+  
+  beforeEach() {
+    let haiku = new Haiku();
+  }
+
+  test('should create correctly a haiku object',() => {
+
+  }); 
+});
