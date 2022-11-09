237a6b323178431d9c7309f0235f826397cbaebe
delguanzon <yodelguanzon@gmail.com>
2022-11-09 13:58:48 -0800

    Pass: Separate entry into three lines
    Co-authored-by: VW <vera.weikel@gmail.com>
    Co-authored-by: Jennifer Holcomb <jenniferlholcomb@gmail.com>
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -4,14 +4,14 @@ describe('Haiku', () => {
   let haiku;
   
   beforeEach(() => {
-    haiku = new Haiku("I write, erase, rewrite\nErase again, and then\n A poppy blooms");
+    haiku = new Haiku("I write, erase, rewrite\nErase again, and then\nA poppy blooms");
   });
 
   test('should create correctly a haiku object',() => {
-    expect(haiku.entry).toEqual("I write, erase, rewrite\nErase again, and then\n A poppy blooms");
+    expect(haiku.entry).toEqual("I write, erase, rewrite\nErase again, and then\nA poppy blooms");
   }); 
 
-  test('should separe our haiku object into three lines', () => {
+  test('should separate our haiku object into three lines', () => {
     expect(haiku.line1).toEqual("I write, erase, rewrite");
     expect(haiku.line2).toEqual("Erase again, and then");
     expect(haiku.line3).toEqual("A poppy blooms");
--- src/js/haiku.js
+++ src/js/haiku.js
@@ -2,5 +2,8 @@ export default class Haiku {
   
   constructor(entry) {
     this.entry = entry;
+    this.line1 = entry.split('\n')[0];
+    this.line2 = entry.split('\n')[1];
+    this.line3 = entry.split('\n')[2];
   }
 }
